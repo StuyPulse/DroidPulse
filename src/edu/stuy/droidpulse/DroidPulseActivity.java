134cd4d44ca92735b79b9baaefd1c8831a51916f
Yulli Equine <yulli@yulli.org>
2012-02-21 15:25:47 -0500

    initial commit

---
+++ src/edu/stuy/droidpulse/DroidPulseActivity.java
@@ -0,0 +1,26 @@
+package edu.stuy.droidpulse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+
+public class DroidPulseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final TextView tv = (TextView) findViewById(R.id.textView1);
+        final Button button = (Button) findViewById(R.id.button1);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				tv.setText("Hello, Android."); //$NON-NLS-1$
+				setContentView(tv);
+			}
+        });
+    }
+}
