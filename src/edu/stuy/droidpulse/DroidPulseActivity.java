c1ee647d356a6ec9f4b7894ddcf2502145f47739
Danny Qiu <dqiu55@gmail.com>
2012-03-07 11:24:39 -0500

    added integrated web browser to show the team info and currently testing how to implement the spinner.

--- src/edu/stuy/droidpulse/DroidPulseActivity.java
+++ src/edu/stuy/droidpulse/DroidPulseActivity.java
@@ -3,8 +3,11 @@ package edu.stuy.droidpulse;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class DroidPulseActivity extends Activity {
     /** Called when the activity is first created. */
@@ -13,14 +16,27 @@ public class DroidPulseActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final TextView tv = (TextView) findViewById(R.id.textView1);
-        final Button button = (Button) findViewById(R.id.button1);
-        
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button aboutbutton = (Button) findViewById(R.id.welcome_aboutbutton);
+        aboutbutton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				tv.setText("Hello, Android."); //$NON-NLS-1$
-				setContentView(tv);
+				setContentView(R.layout.webview_aboutus);
+				WebView webView = (WebView) findViewById(R.id.webview_aboutus);
+			    webView.loadUrl("http://stuypulse.com/aboutus");
+			    webView.getSettings().setJavaScriptEnabled(true);
+			    webView.setWebViewClient(new WebViewClient());
 			}
         });
+        
+        final Button robotbutton = (Button) findViewById(R.id.welcome_robotbutton);
+        robotbutton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			    setContentView(R.layout.robot);
+			    String robots[] = {"2011 - Desdroid","2010 - Donovan","2009 - Michael 1","2008 - Desbot","2007 - Tom","2006 - Joshua","2005 - Yvette"};
+			    Spinner spinner = (Spinner) findViewById(R.id.robot_spinner);
+			    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(null, android.R.layout.simple_spinner_item, robots);
+			    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			    spinner.setAdapter(spinnerArrayAdapter);
+			}
+		});
     }
 }
